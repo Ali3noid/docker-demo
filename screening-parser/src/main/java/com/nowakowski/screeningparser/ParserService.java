dd2438537b8faefa18d94cefe6eba42322bed64b
Rafal Nowakowski <rafal1nowakowski@gmail.com>
2018-03-24 00:14:14 +0100

    structure - created package screening-api with screening POJO - wire modules in POM - quick visibility test of connection

---
+++ screening-parser/src/main/java/com/nowakowski/screeningparser/ParserService.java
@@ -0,0 +1,13 @@
+package com.nowakowski.screeningparser;
+
+import com.nowakowski.model.Screening;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParserService {
+
+    public void useScreeningApi(){
+        List<Screening> screeningList = new ArrayList<>();
+    }
+}
