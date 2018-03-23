44c7837b5cfd9f4556a7045f9268e351d807e6d1
Rafal Nowakowski <rafal1nowakowski@gmail.com>
2018-03-23 23:15:43 +0100

    structure - renaming of screeningService - added spring boot to screening-parser

---
+++ screening-parser/src/main/java/com/nowakowski/screeningparser/ScreeningParserApplication.java
@@ -0,0 +1,12 @@
+package com.nowakowski.screeningparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreeningParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreeningParserApplication.class, args);
+	}
+}
